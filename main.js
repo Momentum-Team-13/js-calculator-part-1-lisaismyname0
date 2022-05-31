c1ebce92eb0abe2ea78faef91c100e58015546fe
Lisa Williams <lisaismyname0@gmail.com>
2022-05-30 21:39:05 -0400

    X on calculator translates to * in display, updated notes/attempts

--- main.js
+++ main.js
@@ -7,26 +7,23 @@ const clickdot = document.getElementById("dot")
 let result = document.createElement("div");
 const display = document.querySelector("#display")
 
-
-console.log(buttons[0].classList);
-
 for (let button of buttons) {
     button.addEventListener("click", function (event) {
         console.log(event.target.textContent);
         display.appendChild(result);
-        
-        if (event.target.id === "clickdot" && display.innerText.length === 0){
+
+        if (event.target.id === "clickdot" && display.innerText.length === 0) {
             result.innerText = 0.;
         }
 
-        if (display.innerText.length >= 10){
+        if (display.innerText.length >= 10) {
             display.style.fontSize = "medium";
-        } 
-        
-        if (display.innerText.length >= 20){
-            display.style.fontSize = "small"
         }
 
+        if (display.innerText.length >= 20) {
+            display.style.fontSize = "small"
+        }
+        
         if (event.target.id === "equals") {
             display.appendChild(result);
             result.innerText = math.eval(display.innerText);
@@ -36,65 +33,64 @@ for (let button of buttons) {
             result.innerText = "   ";
         }
         
-        else if (event.target.id === "multiplier") {
-            result.innerText = eval(display.innerText)
-            display.appendChild(result);
+        else if (event.target.id === "multiply") {
+            result.innerText += "*"
         }
-        
+            
         else {
-            result.innerText += event.target.textContent;
-        }
+        result.innerText += event.target.textContent;
+            }
         
     });
 }
 
+
 document.addEventListener('keydown', (event) => {
-    if (event.key == 0) {display.append("0")};
-    if (event.key == 1) {display.append("1")};
-    if (event.key == 2) {display.append("2")};
-    if (event.key == 3) {display.append("3")};
-    if (event.key == 4) {display.append("4")};
-    if (event.key == 5) {display.append("5")};
-    if (event.key == 6) {display.append("6")};
-    if (event.key == 7) {display.append("7")};
-    if (event.key == 8) {display.append("8")};
-    if (event.key == 9) {display.append("9")};
-    if (event.key == "+") {display.append("+")};
-    if (event.key == "-") {display.append("-")};
-    if (event.key == "/") {display.append("/")};
-    if (event.key == "*") {display.append("*")};
-    if (display.innerText.length >= 10){
-        display.style.fontSize = "medium";
-    } 
-    if (display.innerText.length >= 20){
-        display.style.fontSize = "small"
-    }
-});
+    if (event.key == 0) { display.append("0") };
+    if (event.key == 1) { display.append("1") };
+    if (event.key == 2) { display.append("2") };
+    if (event.key == 3) { display.append("3") };
+    if (event.key == 4) { display.append("4") };
+    if (event.key == 5) { display.append("5") };
+    if (event.key == 6) { display.append("6") };
+    if (event.key == 7) { display.append("7") };
+    if (event.key == 8) { display.append("8") };
+    if (event.key == 9) { display.append("9") };
+    if (event.key == "+") { display.append("+") };
+    if (event.key == "-") { display.append("-") };
+    if (event.key == "/") { display.append("/") };
+    if (event.key == "*") { display.append("*") };
+    //I tried to play with getting the delete button to remove the last item from the string in the display:
+    
+    // let string = display.innerText.length
+    // if (event.key == "Backspace" || event.key == "Delete") {
+        //     string.splice(0, -1);
+        //     console.log("deleted!")}
+        
+        if (display.innerText.length >= 10) {
+            display.style.fontSize = "medium";
+        }
+        if (display.innerText.length >= 20) {
+            display.style.fontSize = "small"
+        }
+    });
+    
+// the key press event listener works but the numbers in the field don't show up level with the numbers that are pressed on the calculator) this is is another concept I believe could use the switch method maybe using something like the following:
+    // document.addEventListener('keydown', (event) => {
+        //     const keyPr = event.key;
+        //     switch (keyPr){
+            //     case event.key = 0:
+            //         display.append("0");
+            //         break;
+            //     case event.key = 1:
+            //         display.append("1");
+            // }
+            // });
+        
+//ADDITIONAL NOTES/TODO
 
+// if I want to use further classifications, here are the variables for my operator & number buttons... i began to attempt this with my multiply and divider ids:
 
-//I tried to play with getting the delete button to remove the last item from the string in the display:
-
-// if (event.key == "Backspace" || event.key == "Delete") {
-//     display.innerText.string.splice(0, -1);
-//     console.log("deleted!")}
-
-// the key press event listener is another concept I believe could use the switch method
-// document.addEventListener('keydown', (event) => {
-//     const keyPr = event.key;
-//     switch (keyPr){
-//     case event.key = 0:
-//         display.append("0");
-//         break;
-//     case event.key = 1:
-//         display.append("1");
-// }
-// });
-
-// TODO
-
-
-            // if I want to use further classifications, here are the variables for my operator & number buttons... i began to attempt this with my multiply and divider ids:
-            
 // let operators = document.querySelectorAll(".operator");
 
 // for (let operator of operators) {
@@ -107,3 +103,17 @@ document.addEventListener('keydown', (event) => {
 // }
 // let numbers = document.querySelectorAll(".number");
 // console.log(numbers);
+
+
+// Attempts at including error messages / disallowing invalid inputs (these also seem to respond differently if I have math.eval() vs eval()enabled):
+                        
+// if (console.log.includes = "Uncaught SyntaxError") { alert("impossible!") }
+// // ^ this causes an error message as soon as I reload the page
+                        
+// if (display.innerText.includes(".." || "//" || "-+" || "\+")) {
+//     alert("Invalid Input!")
+// }
+                            
+// if (math.eval(display.innerText) = NaN) {result.innerText 
+
+// }
